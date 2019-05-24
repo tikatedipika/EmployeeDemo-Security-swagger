88ea1f4755a1e1785238510375e784ae94424971
tikatedipika <tikate.dipika@gmail.com>
2019-05-24 18:13:15 -0400

    removied unnecessary imports

--- src/main/java/com/cognizant/demo/EmployeeDemo/Repositories/EmployeeRepo.java
+++ src/main/java/com/cognizant/demo/EmployeeDemo/Repositories/EmployeeRepo.java
@@ -2,7 +2,6 @@ package com.cognizant.demo.EmployeeDemo.Repositories;
 
 import com.cognizant.demo.EmployeeDemo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
