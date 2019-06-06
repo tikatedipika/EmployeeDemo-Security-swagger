3d8ea14a531691e6583a2f37f15c3eff4e350db0
ajnijv81 <ajnijv81@gmail.com>
2019-06-06 07:23:15 -0400

    adding mysql changes

--- src/main/java/com/cognizant/demo/EmployeeDemo/CORSFilter.java
+++ src/main/java/com/cognizant/demo/EmployeeDemo/CORSFilter.java
@@ -9,7 +9,7 @@ import org.springframework.web.server.WebFilterChain;
 public class CORSFilter implements WebFilter {
     @Override
     public reactor.core.publisher.Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        exchange.getResponse().getHeaders().set("Access-Control-Allow-Methods","POST");
+        //exchange.getResponse().getHeaders().set("Access-Control-Allow-Methods","POST");
         exchange.getResponse().getHeaders().set("Access-Control-Allow-Origin","");
         return chain.filter(exchange);
     }
